f6f8f627cd2fda31b1e92549f6b510ebedc7cb9e
Faris Widyantho <radenfaris@gmail.com>
2019-05-19 13:59:24 +0700

    setup convert as csv

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const parseArchive = require('./lib')
+const {
+    parseArchive,
+    convertToCsv,
+} = require('./lib')
 
 const pathHtml = './raw_data/Cindy Yuvia (@Cindvia_JKT48)   Twitter.html'
 const username = 'Cindvia_JKT48'
 const fileName = 'result.json'
 const option = {
     save: true,
-    continue: false, //if true will add data (continue adding)
+    continue: true, //if true will add data (continue adding)
     saveAs: 'json',
 }
 
-parseArchive({
-    pathHtml,
-    username,
-    fileName,
-    option,
-})
+if(process.argv[2] == 'convert_to_csv') {
+    convertToCsv({
+        fileName
+    })
+} else {
+    parseArchive({
+        pathHtml,
+        username,
+        fileName,
+        option,
+    })
+}
